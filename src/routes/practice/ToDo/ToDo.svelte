3b524459cc497dc31be37a142f921c26c0d14e0e
okorion <ok.or.orion@gmail.com>
2023-09-02 22:57:12 +0900

    23.09.02 feat: PRACTICE 탭에 ToDo 작성 수정하기, 삭제 기능 추가

--- src/routes/practice/ToDo/ToDo.svelte
+++ src/routes/practice/ToDo/ToDo.svelte
@@ -1,7 +1,40 @@
 <script>
 	export let todo;
+
+	let isEdit = false;
+	let title = '';
+
+	function onEdit() {
+		isEdit = true;
+		title = todo.title;
+	}
+	function offEdit() {
+		isEdit = false;
+	}
+	function updateToDo() {
+		todo.title = title;
+		offEdit();
+	}
+	function deleteToDo() {
+		todos = todos.filter((t) => t.id !== todo.id);
+	}
 </script>
 
-<div>
-	{todo.title}
-</div>
+{#if isEdit}
+	<div>
+		<input
+			bind:value={title}
+			type="text"
+			on:keydown={(e) => {
+				e.key === 'Enter' && updateToDo();
+			}}
+		/>
+		<button on:click={updateToDo}> 수정완료 </button>
+		<button on:click={offEdit}> 취소 </button>
+	</div>
+{:else}
+	<div>
+		{todo.title}
+		<button on:click={onEdit}> 수정하기 </button>
+		<button on:click={deleteToDo}> 삭제 </button>
+	</div>{/if}
