b3c2c56abb163dea12e4017c188da9159b548b45
okorion <ok.or.orion@gmail.com>
2023-09-02 22:40:10 +0900

    23.09.02 feat: PRACTICE 탭에 ToDo 작성 기능 추가

--- src/routes/practice/+page.svelte
+++ src/routes/practice/+page.svelte
@@ -1,8 +1,35 @@
 <script>
+	import ToDo from './ToDo/ToDo.svelte';
+
 	let name = 'world';
+	let title = '';
+	let todos = [];
+	let id = 0;
+
+	function createToDo() {
+		todos.push({ id, title });
+		todos = todos; // 할당을 통해 반응성 추가
+		title = '';
+		id++;
+	}
 </script>
 
 <h1>Hello {name}!</h1>
 
+<div class="board">
+	<input bind:value={title} type="text" />
+	<button on:click={createToDo}> 작성하기 </button>
+</div>
+
+{#each todos as todo}
+	<div>
+		<ToDo {todo} />
+	</div>
+{/each}
+
 <style>
+	.board {
+		display: flex;
+		flex-direction: row;
+	}
 </style>
