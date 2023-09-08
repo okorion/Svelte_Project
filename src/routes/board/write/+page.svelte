868f36195834164a0b0ad8ca45a8467a3a8f725e
okorion <ok.or.orion@gmail.com>
2023-09-09 02:00:37 +0900

    23.09.09 Add title, content 추가

--- src/routes/board/write/+page.svelte
+++ src/routes/board/write/+page.svelte
@@ -5,6 +5,9 @@
 </script>
 
 <h1>Write 라우팅 적용 성공!</h1>
+<input type="title" placeholder="제목">
+<input type="content" name="" id="" placeholder="내용">
+<button>작성하기</button>
 
 <button on:click={back}>뒤로가기</button>
 
