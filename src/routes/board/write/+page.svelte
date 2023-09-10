fdc91f1e0dbaaba531069cc14dad1f85d3c286ed
okorion <ok.or.orion@gmail.com>
2023-09-10 18:12:32 +0900

    feat: 작성하기 클릭 시 Alert 기능과 history back 기능 추가

--- src/routes/board/write/+page.svelte
+++ src/routes/board/write/+page.svelte
@@ -1,4 +1,9 @@
 <script>
+	function write() {
+		alert("작성완료");
+		history.back();
+	}
+	
 	function back() {
 		history.back();
 	}
@@ -7,7 +12,7 @@
 <h1>Write 라우팅 적용 성공!</h1>
 <input type="title" placeholder="제목">
 <input type="content" name="" id="" placeholder="내용">
-<button>작성하기</button>
+<button on:click={write}>작성하기</button>
 
 <button on:click={back}>뒤로가기</button>
 
